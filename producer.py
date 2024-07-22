da9ae73ff62732692e1177804ae37955282bf3c2
Rafael Guimarães <rafael.fguimaraes@dataprev.gov.br>
2024-07-22 15:38:32 -0300

    Add oproducer and consumer

---
+++ producer.py
@@ -0,0 +1,27 @@
+from confluent_kafka import Producer
+
+p = Producer({'bootstrap.servers': 'localhost:9092'})
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+messages = ['Message 1', 'Message 2', 'Message 3']
+
+for message in messages:
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    
+    # Asynchronously produce a message. The delivery report callback will
+    # be triggered from the call to poll() above, or flush() below, when the
+    # message has been successfully delivered or failed permanently.
+    p.produce('messages', message.encode('utf-8'), callback=delivery_report)
+
+# Wait for any outstanding messages to be delivered and delivery report
+# callbacks to be triggered.
+p.flush()
